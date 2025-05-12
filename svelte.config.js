650ff6123677609911b061fbd93a48e9205cf895
404Games <162417562+404GamesGithub@users.noreply.github.com>
2025-05-11 18:01:20 -0700

    anuther fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,31 @@
-kit: {
-	adapter: Number(process.env.DESKTOP) == 1
-	  ? desktopAdapter({
-		  pages: 'build',
-		  assets: 'build',
-		  fallback: 'index.html',
-		  precompress: false,
-		  strict: true
-		})
-	  : adapter({
-		  runtime: 'edge'
-		}),
-	paths: {
-	  base: process.env.NODE_ENV === 'production' ? '/mujay' : '' // e.g., '/mujay'
-	},
-	csrf: false
+import desktopAdapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+  define: {
+    'process.env.DESKTOP': JSON.stringify(process.env.DESKTOP || '0')
+  },
+  kit: {
+    adapter:
+      Number(process.env.DESKTOP) == 1
+        ? desktopAdapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: 'index.html',
+            precompress: false,
+            strict: true
+          })
+        : adapter({
+            runtime: 'edge'
+          }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/mujay' : ''
+    },
+    csrf: false
   }
+};
+
+export default config;
